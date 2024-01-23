6f6b5335084ad093e20237785f51f161b31c6234
Viviane Mezzomo <viviane.mezzomo@codex.com.br>
2024-01-22 22:30:01 -0300

    README e desafio de conversão de temperatura.

---
+++ README.md
@@ -0,0 +1,4 @@
+# Java: criando a sua primeira aplicação
+
+### Descrição:
+Exercícios realizados dentro do curso ' _Java: criando a sua primeira aplicação_ ', promovido pela **[Alura](https://www.alura.com.br/)**. 
---
+++ screenMatch/src/desafioTemperatura.java
@@ -0,0 +1,17 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class desafioTemperatura {
+    public static void main(String[] args){
+        Scanner leitura = new Scanner(System.in);
+        System.out.println("Digite a temperatura em graus Celcius que deseja converter:");
+        double temperaturaCelcius = leitura.nextInt();
+        double tempraturaFahrenheit = (temperaturaCelcius * 1.8) + 32;
+        String mensagem = String.format("A temperatura de %.2f Celsius é equivalente a %.2f Fahrenheit", temperaturaCelcius, tempraturaFahrenheit);
+        System.out.println(mensagem);
+
+        int tempraturaFahrenheitInt = (int) (tempraturaFahrenheit);
+        String mensagem2 = String.format("A temperatura em valor inteiro é de %d Fahrenheit", tempraturaFahrenheitInt);
+        System.out.println(mensagem2);
+    }
+}
